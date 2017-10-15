aeaa506f96f06fdc9277bb41c631ee914892b4ab
Shei <chegeeric66@gmail.com>
2017-10-15 11:31:44 +0300

    initial Express Scafolding

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+
+/* GET home page. */
+router.route('/')
+      .get((req, res, next)=>{
+       res.render('index', { title: 'Music List Alpha' });
+      });
+
+
+module.exports = router;
